49259a62ea5183b09fc2327b47dcd6cc1b407bc1
Marcela P Proenca <marcelaproencaeng@gmail.com>
2023-09-25 19:36:05 -0300

    Configurando Tela Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios';
+import Granito_Marmore from "./assets/Imagens/Cacos_De_Granito_e_Marmore.jpg"
+import { Formulario } from './Formulario';
 
 function App() {
   const [autenticado, setAutenticado] = useState(false);
   const [usuario,setUsuario] = useState(null);
   const [senha,setSenha] = useState(null);
   const [mensagemErro,setMensagemErro] = useState(null);
+  const [exibirCadastro,setexibirCadastro]=useState(false);
 
 
 
   const handleOnSubmit = (event) => {
     event.preventDefault();
     axios
-      .post('http://localhost:8080/api/v1/logins', {userName:usuario , senha })
+      .post('http://localhost:8080/api/v1/logins', {userName:usuario , password:senha })
       .then(() => setAutenticado(true))
       .catch(() => setMensagemErro(true))
   }
@@ -24,7 +27,11 @@ function App() {
   //capturar os valores dos inputs usuario e senha 
   //enviar os dados
   //endpoint no java para a rota
-
+if(exibirCadastro){
+  return  <Formulario onClick={()=>{
+    setexibirCadastro(false)
+  }}></Formulario>
+}
   if(autenticado === false) {
     return (
       <>
@@ -44,7 +51,12 @@ function App() {
           <div>
            <button onClick={() => handleOnSubmit()}>Logar </button>
            <button id="Excluir" onClick={() => setTarefas(tarefas)}>
+
         Excluir
+        </button>
+        <button id="Cadastra-se" onClick={() => setexibirCadastro(true)}>
+            
+        Cadastra-se
         </button>
           </div>
           </form>
@@ -52,81 +64,37 @@ function App() {
       </>
     )
   }
+const dados =[
+  {
+    title:"Cacos de Granito e Mármore",
+    img:"https://i.imgur.com/DQQBRtl.jpg",
+    description:"Vendo Cacos de Granito e Mármore",
+    name:"Cacos de Granito e Mármore",
+    distance:"20m",
+    place:"Praia Grande-SP",
+    date:"08 de set"
 
+
+  }
+]
   return (
    
     <div>
-      <div><img src="Imagens/Cacos_De_Granito_e_Marmore.jpg"
-                title="Cacos de Granito e Mármore"
+      {
+        dados.map(dado =>(
+          <div><img src={dado.img}
+                title={dado.title}
                 width="200" height="100"></img>
-        <ul><li>Vendo Cacos de Granito e Mármore</li>
-            <li>Cacos de Granito e Mármore-Construção e </li>
-               <li>20m</li>
-               <li>Praia Grande-SP  08 de set</li>
+        <ul><li>{dado.description}</li>
+            <li>{dado.name}</li>
+               <li>{dado.distance}</li>
+               <li>{dado.place} {dado.date}</li>
         </ul>
       </div>
-      <div><img src="Imagens/Pedrisco.jpg"
-                title="Pedrisco"
-                width="200" height="200"></img>
-        <ul><li>Vendo Pedrisco-Construção e Demolição</li>
-            <li>Pedrisco-Construção e Demolição</li>
-               <li>1000m</li>
-               <li>Valinhos-SP 20 out</li>
-        </ul>
-      </div>
-      <div><img src="Imagens/Agregado_EVA.jpg"
-                title="Agregado Leve de E.V.A"
-                width="200" height="200"></img>
-        <ul><li>Vendo Agregado Leve de E.V.A</li>
-               <li>Agregado de Construção e Demolição</li>
-               <li>2000kg</li>
-               <li>Estância Velha-RS 27 Ago</li>
-            </ul>
-      </div>
-      <div><img src="Imagens/Nego_De_Fumo.jpg"
-                title="Nego De Fumo"
-                width="200" height="200"></img>
-        <ul><li>Vendo Nego de Fumo </li>
-               <li>Nego De Fumo-Químicos</li>
-               <li>300.000Kg</li>
-               <li>Suzano-SP 29 Jun</li>
-            </ul>
-      </div>
-      <div><img src="Imagens/Flake_Azul_e_Verde_Plástico.jpg"
-                title="Plástico Flake Azul e Verde"
-                width="200" height="200"></img>
-        <ul><li>Vendo Plástico Flake Azul e Verde</li>
-               <li>Flake Azul e Verde-Plástico</li>
-               <li>60.000Kg</li>
-               <li>Maracanaú-CE 25 Jun</li>
-            </ul>
-      </div>
-      <div><img src="Imagens/Fibra_De_Vidro.jpg"
-                title="Fibra de Vidro Desfiada"
-                width="200" height="200"></img>
-        <ul><li>Vendo Fibra de Vidro Desfiada</li>
-               <li>Fibra de Vidro Desfiada- Construção e Demolição</li>
-               <li>500.000Kg</li>
-               <li>Garibaldi-RS</li>
-            </ul>
-      </div>
-      <div><img src="Imagens/Areia_De_Fundicao.jpg"
-                title="Areia de Fundição"
-                width="200" height="200"></img>
-        <ul><li>Vendo Areia de Fundição</li>
-               <li>Areia de Fundição-Construção e Demolição</li>
-               <li>1260.000Kg</li>
-               <li>Santo Ângelo-RS 23 Jan</li>
-        </ul>
-      </div>
-      <div><img src="Imagens/Borracha_Silicone_Em_Po.jpg"
-                title="Borracha Silicone em Pó"
-                width="200" height="200"></img>
-        <ul><li>Vendo Borracha Silicone em Pó-Químicos</li>
-            <li>60.000Kg</li>
-            <li>São caetano do Sul -SP 20 Out</li>
-        </ul>
-      </div>
+
+        ))
+      }
+     
           
       <button onClick={()=>{
         setAutenticado(false)
---
+++ src/Formulario.jsx
@@ -0,0 +1,10 @@
+export const Formulario=({onClick})=>{
+return (
+    <div><h1>Cadastro</h1>
+     <button id="Voltar" onClick={onClick}>
+            
+            Voltar
+            </button>
+    </div>
+)
+}
