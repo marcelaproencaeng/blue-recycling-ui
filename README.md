1e625fb74579b190c2cc872dca0e5e31a03e394f
Marcela P Proenca <marcelaproencaeng@gmail.com>
2023-09-27 20:07:52 -0300

    Adicionando propriedades no Formulário

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Blue Recycling
 
 npm run dev
+
+
+Formulário
+
+    private String razaoSocial;
+    private String cnpj;
+    private String ResiduoDeInteresse;
+    private Boolean isVendedor;
+    private Boolean hasTransporte;
+    private String email;
--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,6 +1,32 @@
 export const Formulario=({onClick})=>{
+   // private String razaoSocial;
+    //private String cnpj;
+   // private String ResiduoDeInteresse;
+   // private Boolean isVendedor;
+   // private Boolean hasTransporte;
+   // private String email;
+
+
 return (
     <div><h1>Cadastro</h1>
+    <form action="">
+        <label htmlFor="razaoSocial">Razão Social</label>
+        <input type="text"id="razaoSocial" />
+        <label htmlFor="cnpj">Cnpj</label>
+        <input type="text"id="cnpj"/>
+        <p>Você é vendedor?</p>
+        <input type="radio"id="sim" name="vendedor"/>
+        <label htmlFor="sim">sim</label>
+        <input type="radio" id="nao" name="vendedor"/>
+        <label htmlFor="nao">não</label>
+        <p>Você tem transporte?</p>
+        <input type="radio" id="sim" name="transporte"/>
+        <label htmlFor="sim">sim</label>
+        <input type="radio" id="nao" name="transporte"/>
+        <label htmlFor="nao">não</label>
+
+
+    </form>
      <button id="Voltar" onClick={onClick}>
             
             Voltar
