71618145fb54a8eb686e5449d066e65c1812feef
Marcela P Proenca <marcelaproencaeng@gmail.com>
2023-09-27 02:01:28 -0300

    Listando Resíduos

--- src/App.jsx
+++ src/App.jsx
@@ -64,10 +64,27 @@ if(exibirCadastro){
       </>
     )
   }
+  const logos =[
+    {
+    img:"https://i.imgur.com/H6NHjs0.jpg"
+
+    }
+]
+//return(
+ // <div>
+ // {
+  //  logos.map(logo =>(
+   //   <div><img src={logo.img}
+  //    width="200" height="200"></img>
+  //    </div>
+//))
+ // }
+
+
 const dados =[
   {
     title:"Cacos de Granito e Mármore",
-    img:"https://i.imgur.com/DQQBRtl.jpg",
+    img:"https://i.imgur.com/mSCMpek.jpg",
     description:"Vendo Cacos de Granito e Mármore",
     name:"Cacos de Granito e Mármore",
     distance:"20m",
@@ -75,9 +92,75 @@ const dados =[
     date:"08 de set"
 
 
+  },
+  {
+    title:"Agregado Leve de E.V.A",
+    img:"https://i.imgur.com/LHo30LN.jpg",
+    description:"Vendo Agregado Leve de E.V.A",
+    name:"Agregado de Construção de Demolição",
+    distance:"2000Kg",
+    place:"Estância Velha-RS",
+    date:"27 de Ago"
+  },
+  {
+    title:"Areia de Fundição",
+    img:"https://i.imgur.com/xVZ7Yit.jpg",
+    description:"Vendo Areia de Fundição",
+    name:"Areia de Fundição-Construção e Demolição",
+    distance:"1260Kg",
+    place:"Santo Ângelo-RS",
+    date:"23 de Jan"  
+  },
+  {
+    title:"Borracha Silicone em Pó",
+    img:"https://i.imgur.com/vsxZ5SP.jpg",
+    description:"Vendo Borracha Silicone em Pó",
+    name:"Borracha Silicone em Pó-Químicos",
+    distance:"60.000Kg",
+    place:"São caetano do Sul -SP",
+    date:"20 de Out"
+  },
+  {
+    title:"Nego De Fumo",
+    img:"https://i.imgur.com/BkiBSN6.jpg",
+    description:"Vendo Nego de Fumo",
+    name:"Nego de Fumo-Químicos",
+    distance:"300.000Kg",
+    place:"Suzano -SP",
+    date:"29 de Jun"
+  },
+  {
+    title:"Plástico Flake Azul e Verde",
+    img:"https://i.imgur.com/sdFNlCf.jpg",
+    description:"Vendo Plástico Flake Azul e Verde",
+    name:"Flake Azul e Verde-Plástico",
+    distance:"60.000Kg",
+    place:"Maracanaú-CE",
+    date:"25 Jun"
+  },
+  {
+    title:"Fibra de Vidro Desfiada",
+    img:"https://i.imgur.com/JnHBDnt.jpg",
+    description:"Vendo Fibra de Vidro Desfiada",
+    name:"Fibra de Vidro Desfiada- Construção e Demolição",
+    distance:"500.000Kg",
+    place:"Garibaldi-RS",
+    date:"20 Jan"
+  },
+  {
+    title:"Pedrisco",
+    img:"https://i.imgur.com/XTDadKe.jpg",
+    description:"Vendo Pedrisco",
+    name:"Pedrisco-Construção e Demolição",
+    distance:"1000m",
+    place:"Valinhos",
+    date:"20 out"
   }
+      
+  
 ]
   return (
+    
    
     <div>
       {
@@ -94,6 +177,7 @@ const dados =[
 
         ))
       }
+   
      
           
       <button onClick={()=>{
